e3e9d182292305c56a371ddb192f4e6f3eed3e2e
Igor Moreira <igormsantos1@icloud.com>
2022-08-31 19:48:04 -0300

    inserção inicial

---
+++ templates/footer.php
@@ -0,0 +1,21 @@
+<!--<footer class="text-center text-white" style="background-color: #007BFF;">
+
+  <div class="container p-4">
+ 
+    <section class="">
+            </iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+   
+  </div>
+ 
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright:
+    <a class="text-white" href="https://www.e-farsas.com/wp-content/uploads/ayu5.jpg">Agendaralha</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+</body>
+</html>
